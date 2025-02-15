d4acca3f2fe55ae6873c009a0562275201d21e57
Aniketh <techrunstheworld@gmail.com>
2025-02-15 14:08:27 +0530

    Initial Setup

---
+++ src/app/components/background-beams-with-collision.tsx
@@ -0,0 +1,163 @@
+"use client";
+import { cn } from "@lib/utils";
+import { motion, AnimatePresence } from "framer-motion";
+import React, { useRef, useState, useEffect } from "react";
+
+export const BackgroundBeamsWithCollision = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  const containerRef = useRef<HTMLDivElement>(null!);
+  const parentRef = useRef<HTMLDivElement>(null!);
+
+  const beams = [
+    {
+      initialX: 10,
+      translateX: 10,
+      duration: 7,
+      repeatDelay: 3,
+      delay: 2,
+    },
+    {
+      initialX: 600,
+      translateX: 600,
+      duration: 3,
+      repeatDelay: 3,
+      delay: 4,
+    },
+    {
+      initialX: 100,
+      translateX: 100,
+      duration: 7,
+      repeatDelay: 7,
+      className: "h-6",
+    },
+    {
+      initialX: 400,
+      translateX: 400,
+      duration: 5,
+      repeatDelay: 14,
+      delay: 4,
+    },
+    {
+      initialX: 800,
+      translateX: 800,
+      duration: 11,
+      repeatDelay: 2,
+      className: "h-20",
+    },
+    {
+      initialX: 1000,
+      translateX: 1000,
+      duration: 4,
+      repeatDelay: 2,
+      className: "h-12",
+    },
+    {
+      initialX: 1200,
+      translateX: 1200,
+      duration: 6,
+      repeatDelay: 4,
+      delay: 2,
+      className: "h-6",
+    },
+  ];
+
+  return (
+    <div
+      ref={parentRef}
+      className={cn(
+        "h-96 md:h-[40rem] bg-gradient-to-b from-white to-neutral-100 dark:from-neutral-950 dark:to-neutral-800 relative flex items-center w-full justify-center overflow-hidden",
+        className
+      )}
+    >
+      {beams.map((beam, index) => (
+        <CollisionMechanism
+          key={index}
+          beamOptions={beam}
+          containerRef={containerRef}
+          parentRef={parentRef}
+        />
+      ))}
+
+      {children}
+      <div
+        ref={containerRef}
+        className="absolute bottom-0 bg-neutral-100 w-full inset-x-0 pointer-events-none"
+        style={{
+          boxShadow:
+            "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset",
+        }}
+      ></div>
+    </div>
+  );
+};
+
+const CollisionMechanism = ({
+  containerRef,
+  parentRef,
+  beamOptions = {},
+}: {
+  containerRef: React.RefObject<HTMLDivElement>;
+  parentRef: React.RefObject<HTMLDivElement>;
+  beamOptions?: {
+    initialX?: number;
+    translateX?: number;
+    initialY?: number;
+    translateY?: number;
+    rotate?: number;
+    className?: string;
+    duration?: number;
+    delay?: number;
+    repeatDelay?: number;
+  };
+}) => {
+  const beamRef = useRef<HTMLDivElement>(null);
+  const [collision, setCollision] = useState(false);
+
+  useEffect(() => {
+    const checkCollision = () => {
+      if (beamRef.current && containerRef.current) {
+        const beamRect = beamRef.current.getBoundingClientRect();
+        const containerRect = containerRef.current.getBoundingClientRect();
+        
+        if (beamRect.bottom >= containerRect.top) {
+          setCollision(true);
+          setTimeout(() => setCollision(false), 2000);
+        }
+      }
+    };
+
+    const interval = setInterval(checkCollision, 50);
+    return () => clearInterval(interval);
+  }, [containerRef]);
+
+  return (
+    <motion.div
+      ref={beamRef}
+      animate={{
+        translateY: beamOptions.translateY || "1800px",
+        translateX: beamOptions.translateX || "0px",
+      }}
+      initial={{
+        translateY: beamOptions.initialY || "-200px",
+        translateX: beamOptions.initialX || "0px",
+      }}
+      transition={{
+        duration: beamOptions.duration || 8,
+        repeat: Infinity,
+        repeatType: "loop",
+        ease: "linear",
+        delay: beamOptions.delay || 0,
+        repeatDelay: beamOptions.repeatDelay || 0,
+      }}
+      className={cn(
+        "absolute left-0 top-20 m-auto h-14 w-px rounded-full bg-gradient-to-t from-indigo-500 via-purple-500 to-transparent",
+        beamOptions.className
+      )}
+    />
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,53 @@
+"use client";
+import React from "react";
+import { BackgroundGradient } from "./components/ui/background-gradient";
+import { IconShoppingCart } from "@tabler/icons-react";
 import Image from "next/image";
+import { BackgroundBeamsWithCollision } from "./components/background-beams-with-collision";
 
-export default function Home() {
+export default function BackgroundGradientCard() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <BackgroundBeamsWithCollision className="min-h-screen flex flex-col justify-center items-center" >
+      <h2 className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
+        <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
+          <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+            <span className=""></span>
+          </div>
+          <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
+            <span className=""></span>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </h2>
+      <div className="flex flex-row justify-center items-center gap-4 mt-10">
+        <BackgroundGradient className="rounded-xl w-72 p-4 bg-gray-100 dark:bg-zinc-800 shadow-lg">
+          <div className="flex flex-col items-center">
+            <Image src={`/nike.jpg`} alt="jordans" height={300} width={170} className=" w-[170px] h-[200px] object-contain" />
+            <p className="text-base font-semibold text-black mt-3 dark:text-white text-center">
+              Air Jordan 4 Retro Reimagined.
+            </p>
+            <p className="text-xs text-gray-600 text-center dark:text-gray-400 mt-2">
+              The Air Jordan 4 Retro Reimagined Bred will release on February 17, 2025.
+            </p>
+            <button className="mt-4 flex items-center gap-2 bg-red-600 text-white px-4 py-1.5 rounded-full text-xs font-bold hover:bg-red-500">
+              <IconShoppingCart size={16} /> Buy Now <span className="bg-white text-red-600 px-2 py-0.5 rounded-full text-xs">$100</span>
+            </button>
+          </div>
+        </BackgroundGradient>
+        <BackgroundGradient className="rounded-2xl w-72 p-4 bg-gray-100 dark:bg-zinc-800 shadow-lg">
+          <div className="flex flex-col items-center">
+            <Image src={`/puma.jpg`} alt="puma" height={200} width={170} className=" w-[170px] h-[200px] object-contain" />
+            <p className="text-base font-semibold text-black mt-3 dark:text-white text-center">
+              Puma RS-X Reinvented.
+            </p>
+            <p className="text-xs text-gray-600 text-center dark:text-gray-400 mt-2">
+              The Puma RS-X Reinvented features a bold, futuristic design with retro influences.
+            </p>
+            <button className="mt-4 flex items-center gap-2 bg-blue-600 text-white px-4 py-1.5 rounded-full text-xs font-bold hover:bg-blue-500">
+              <IconShoppingCart size={16} /> Buy Now <span className="bg-white text-blue-600 px-2 py-0.5 rounded-full text-xs">$90</span>
+            </button>
+          </div>
+        </BackgroundGradient>
+      </div>
+    </BackgroundBeamsWithCollision>
   );
 }
